6afde5fde8b43bcd67bf87e3e1388f5f2ccba4f7
unknown <ninetyskid.abc@gmail.com>
2019-10-18 17:28:02 +0600

    (prompt,var,alert)js exercise done

---
+++ js ecercise/script.js
@@ -0,0 +1,7 @@
+console.log ("Hi"); 
+var firstName= prompt("what's your first name");
+var lastName= prompt("what's your last name");
+var age= prompt("what's your age");
+
+console.log("your name is "+firstName +" " +lastName );
+console.log("your age is "+age);
