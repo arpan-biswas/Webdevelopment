1c34e6e691ae3ed40d5beb654a4b125549443c0a
unknown <ninetyskid.abc@gmail.com>
2019-10-25 06:48:08 +0600

    code modified using functions

--- js ecercise/basic todo list/script.js
+++ js ecercise/basic todo list/script.js
@@ -6,14 +6,38 @@ var input = prompt("What would you like to do?");
 		//manage input
 
 		if (input === "list") {
-		console.log(todos);
+			listTodos();
 		}
 		else if (input === "new"){
 		//ask for new todo
-		var newTodo = prompt("Enter new todo");
-		todos.push(newTodo);
+			addTodo();
+		}
+		else if (input === "delete") {
+			delTodo();
 		}
 		//ask againt for input
 		input = prompt("What would you like to do?");
 	}
 	console.log("Okay, you are running away from the console! Nice");
+
+	function listTodos(){
+		console.log("*****************")
+		todos.forEach(function(todo, index){
+			console.log(index+ ": " + todo);
+			})
+		console.log("*****************");
+	}
+
+	function addTodo(){
+		var newTodo = prompt("Enter new todo");
+		todos.push(newTodo);
+		console.log("A todo has been added");
+	}
+
+	function delTodo(){
+		//ask for index to deleted
+		var index = prompt("Enter index of todo to delete");
+		//delete that todo
+		todos.splice(index,1);
+		console.log("Deleted Todo");
+	}
