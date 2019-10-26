3e447d4a4235348d07e156cbb826aacd74af607e
unknown <ninetyskid.abc@gmail.com>
2019-10-26 19:30:01 +0600

    annoymatic website added using js

---
+++ js ecercise/Annoy-o-matic/script.js
@@ -0,0 +1,10 @@
+var ans = prompt ("You there???");
+while (ans.indexOf("yes") == -1  && ans.indexOf("ya") === -1){
+	var ans = prompt ("You here???");
+}
+
+alert("So, you are here!! nice!!!");
+
+
+
+
